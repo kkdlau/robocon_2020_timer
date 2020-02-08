7fecbf8748bd3f2af3b644073fd68c0a1a2666e6
Danny Lau <dannylau@Dannys-Macbook-Pro.local>
2020-02-08 14:39:25 +0800

    Added Button widget

---
+++ lib/widget/white_outline_btn.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class WOutlineButton extends StatelessWidget {
+  @required
+  final Function onPressed;
+  @required
+  final Widget child;
+  const WOutlineButton(
+      {Key key, @required this.onPressed, @required this.child})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return OutlineButton(
+        borderSide: BorderSide(color: Colors.white, width: 2.0),
+        onPressed: onPressed,
+        child: child,
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)));
+  }
+}
